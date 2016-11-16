77c753ca94c12539c7d2407dcc0cc0fbeb327f3f
Tauno Põldoja <tauno.poldoja@tptlive.ee>
2016-11-16 16:01:38 +0200

    update

--- src/com/Chapter6/Ch64.java
+++ src/com/Chapter6/Ch64.java
@@ -5,11 +5,15 @@ public class Ch64 {
     public static double multadd(double a, double b, double c) {
         return (a * b + c);
     }
+//FIX THIS FIX THIS FIX THIS FIX THIS FIX THIS
+    public static double expSum(double sum) {
+        return 1;
+    }
 
     public static void main(String[] args) {
         double a = 1.0;
-        double b = 2.0;
-        double c = 3.0;
+        double b = Math.sin(Math.PI / 4);
+        double c = (Math.cos(Math.PI / 4) / 2);
         multadd(a, b, c);
         System.out.println("The result is " + multadd(a, b, c));
     }
