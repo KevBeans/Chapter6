5da260c3591e855ad847d55cece42a5c491ce467
Tauno Põldoja <tauno.poldoja@tptlive.ee>
2016-11-16 12:50:28 +0200

    completed

--- src/com/Chapter6/Ch61.java
+++ src/com/Chapter6/Ch61.java
@@ -2,7 +2,14 @@ package com.Chapter6;
 
 public class Ch61 {
 
+    public static double calculateArea(double radius) {
+        return Math.PI * radius * radius;
+        //nothing happens - no data is saved
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("boo!" + 7);
+        //("boo!" + 7) works
+        //("boo!")  + 7 returns an error - can't use + in a void method
     }
 }
