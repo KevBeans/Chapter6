01dc8040c2cd37a6b59747fb61eb70ce502ece0b
KevBeans <kevooi.of.estland@gmail.com>
2016-11-17 13:15:03 +0200

    update

--- src/com/Chapter6/Ch64.java
+++ src/com/Chapter6/Ch64.java
@@ -3,20 +3,33 @@ package com.Chapter6;
 public class Ch64 {
 
     public static double multadd(double a, double b, double c) {
+
         return (a * b + c);
     }
 //FIX THIS FIX THIS FIX THIS FIX THIS FIX THIS
-    public static double expSum(double sum) {
-        return 1;
+    public static double expSum(double x) {
+
+        double a = 1.0;
+        double b = x * Math.exp(-x);
+        double c = Math.sqrt(1 - Math.exp(-x));
+        return multadd(a, b, c);
     }
 
     public static void main(String[] args) {
+
         double a = 1.0;
         double b = Math.sin(Math.PI / 4);
-        double c = (Math.cos(Math.PI / 4) / 2);
-        multadd(a, b, c);
-        System.out.println("The first result is " + multadd(a, b, c));
+        double c = Math.cos(Math.PI / 4) / 2;
+        double d = Math.log(10);
+        double e = Math.log(20);
+        double result1 = multadd(a, b, c);
+        double result2 = multadd(a, d, e);
 
-        System.out.println("The second result is " + multadd(a, b, c));
+        multadd(a, b, c);
+        expSum(10);
+
+        System.out.println("The first result of multadd is " + result1);
+        System.out.println("The second result of multadd is " + result2);
+        System.out.println("ExpSum returns " + expSum(10));
     }
 }
