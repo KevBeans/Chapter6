9b281f70a5cebe97d7e83eef68711e41e6e2d224
KevBeans <kevooi.of.estland@gmail.com>
2016-11-17 16:10:34 +0200

    update

--- src/com/Chapter6/Ch67.java
+++ src/com/Chapter6/Ch67.java
@@ -2,13 +2,26 @@ package com.Chapter6;
 
 public class Ch67 {
 
-    public static void main(String[] args) {
-        int n = 5;
-        System.out.println(oddSum(n));
-        System.out.println("The value of n is " + n);
+    public static int oddSum(int n) {
+
+        int base = 1;
+
+        if (n % 2 == 0) {
+        } else {
+            System.out.print(n + " ");
+            if (n <= 0) {
+                return 1;
+            } else {
+                oddSum(n - 1);
+            }
+        }
+        return 1;
     }
 
-    public static int oddSum(int n) {
-        return 1;
+
+    public static void main(String[] args) {
+
+        int n = 50;
+        oddSum(n);
     }
 }
