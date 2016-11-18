51ecd8d4890f072d7e17afc58636d065648e9e68
KevBeans <kevooi.of.estland@gmail.com>
2016-11-18 09:51:38 +0200

    update

--- src/com/Chapter6/Ch67.java
+++ src/com/Chapter6/Ch67.java
@@ -1,27 +1,35 @@
 package com.Chapter6;
 
+import java.util.stream.IntStream;
+
 public class Ch67 {
+
+    static int sum = 0;
 
     public static int oddSum(int n) {
 
         int base = 1;
 
-        if (n % 2 == 0) {
-        } else {
-            System.out.print(n + " ");
-            if (n <= 0) {
-                return 1;
-            } else {
+            if (n % 2 == 0) {
                 oddSum(n - 1);
+            } else {
+                sum = sum + n;
+                System.out.print(n + " ");
+                if (n <= 1) {
+                    return 1;
+                } else {
+                    oddSum(n - 1);
+                }
             }
-        }
-        return 1;
+            return 1;
     }
 
 
     public static void main(String[] args) {
 
-        int n = 50;
-        oddSum(n);
+        System.out.println("Displaying all odd numbers between 1 and input:");
+        oddSum(10);
+        System.out.println();
+        System.out.println("The sum of all odds is " + sum);
     }
 }
