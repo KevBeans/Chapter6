b4254db8dae6a05107fe325ed3e983de6300d022
KevBeans <kevooi.of.estland@gmail.com>
2016-11-18 10:03:06 +0200

    update

--- src/com/Chapter6/Ch69.java
+++ src/com/Chapter6/Ch69.java
@@ -2,6 +2,18 @@ package com.Chapter6;
 
 public class Ch69 {
 
+    public static double power(double x, int n) {
+
+        if Math.pow(x, n) == x * Math.pow(x, n-1) {
+            return Math.pow(x, n);
+        }
+    }
+
     public static void main(String[] args) {
+
+        double x = 4.0;
+        int n = 2;
+
+        power(x, n);
     }
 }
